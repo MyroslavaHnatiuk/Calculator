bebe7d1cf8a7872af656a62fef35f17561e52656
Myroslava Hnatiuk <mira@dreamware.io>
2019-08-21 10:32:25 +0300

    Optimization

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,9 @@
  import React from 'react';
  import { connect } from 'react-redux';
+ import { bindActionCreators } from 'redux';
  import { AddToInput, ClearInput, Add, Minus, Multiplication, Division, Equal } from '../actions/action'; 
  import './App.css';
  import Button from './Button';
- import ClearButton from './ClearButton' ;
-
 
  const mapStateToProps = function ({calculator}){
    return {
@@ -14,31 +13,90 @@
 
  const mapDispatchToProps = dispatch => {
   return {
-    addToInput: (val) => dispatch(AddToInput(val)),
-    clearInput: () => dispatch(ClearInput()),
-    addition: () => dispatch(Add()),
-    minus: () => dispatch(Minus()),
-    multiplication: () => dispatch(Multiplication()),
-    division: () => dispatch(Division()),
-    equal: () => dispatch(Equal()),
+    dispatch,
+    ...bindActionCreators({ AddToInput, ClearInput, Add, Minus, Multiplication, Division, Equal }, dispatch)
   }
 }
 
  let  App = (props) => {
-  const addZeroInput = (val) => {
-     if(props.input) {
-      props.addToInput(val)
-    }
-  }
-
   const addDemical = (val) => {
     if(props.input.indexOf('.') === -1){
-      props.addToInput(val)
+      props.AddToInput(val)
     }
   }
   const addFromKeyBoard = (e) => {
-     props.addToInput(e.target.value)
+     props.AddToInput(e.target.value)
   }
+  const addZeroInput = (val) => {
+    if(props.input) {
+     props.AddToInput(val)
+   }
+ }
+  const buttons = [
+    {
+      item: 7,
+      handler: props.AddToInput
+    }, 
+    {
+      item: 8,
+      handler: props.AddToInput
+    },
+    {
+      item: 9,
+      handler: props.AddToInput
+    },
+    {
+      item: '/',
+      handler: props.Division
+    },
+    {
+      item: 4,
+      handler: props.AddToInput
+    },
+    {
+      item: 5,
+      handler: props.AddToInput
+    },
+    {
+      item: 6,
+      handler: props.AddToInput
+    },
+    {
+      item: '*',
+      handler: props.Multiplication
+    },
+    {
+      item: 1,
+      handler: props.AddToInput
+    },
+    {
+      item: 2,
+      handler: props.AddToInput
+    },
+    {
+      item: 3,
+      handler: props.AddToInput
+    },
+    {
+      item: '+',
+      handler: props.Add
+    },
+    {
+      item: '.',
+      handler: addDemical
+    },
+    {
+      item: 0,
+      handler: addZeroInput
+    },
+    {
+      item: '=',
+      handler: props.Equal
+    },
+    {
+      item: '-',
+      handler: props.Minus
+    }]
 
    return (
      <div className="app">
@@ -47,31 +105,17 @@
        <input  typy="number" className="input" value={props.input} onChange={addFromKeyBoard}/>
        </div>
          <div className='row'>
-           <Button handleClick={props.addToInput}>7</Button>
-           <Button handleClick={props.addToInput}>8</Button>
-           <Button handleClick={props.addToInput}>9</Button>
-           <Button handleClick={props.division}>/</Button>
-         </div>
-         <div className='row'>
-           <Button handleClick={props.addToInput}>4</Button>
-           <Button handleClick={props.addToInput}>5</Button>
-           <Button handleClick={props.addToInput}>6</Button>
-           <Button handleClick={props.multiplication}>*</Button>
-         </div>
-         <div className='row'>
-           <Button handleClick={props.addToInput}>1</Button>
-           <Button handleClick={props.addToInput}>2</Button>
-           <Button handleClick={props.addToInput}>3</Button>
-           <Button handleClick={props.addition}>+</Button>
-         </div>
-         <div className='row'>
-           <Button handleClick={addDemical}>.</Button>
-           <Button handleClick={addZeroInput}>0</Button>
-           <Button handleClick={props.equal}>=</Button>
-           <Button handleClick={props.minus}>-</Button>
+            {buttons.map(
+            (button, index) => 
+              <Button key={index} handleClick={button.handler}>{button.item}</Button>
+            )} 
          </div>
          <div className="row">
-           <ClearButton handleClick={props.clearInput}>Clear</ClearButton>
+         <button 
+          className="button clear"
+          onClick={()=> {props.ClearInput()}}>
+              Clear
+          </button>
          </div>
       
        </div>
